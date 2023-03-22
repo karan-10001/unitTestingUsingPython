ba25539db932292bd7b14ea861c897c081d6665a
karan-10001 <karanpratapsingh10001@gmail.com>
2023-03-22 19:23:34 +0530

    first commit

---
+++ mymodule.py
@@ -0,0 +1,11 @@
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
